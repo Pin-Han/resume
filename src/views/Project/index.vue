9f91462831fcf3e9211070701cb3a60fad142624
Rondo_lin <patrick04184@gmail.com>
2019-06-26 16:02:17 +0800

    upload

--- src/views/Project/index.vue
+++ src/views/Project/index.vue
@@ -1,12 +1,105 @@
 <template>
+  <div class="pos-rel">
+    <div class="span">Project</div>
 
-        <div class="pos-rel">
-            <div class="span">Project</div>
+    <div class="project">
+      <div class="row m-bottom-2">
+        <div class="col-1-of-3">
+          <div class="card">
+            <div class="card__img card__img--1"></div>
+            <div class="card__descript">
+              <div class="card__descript--title">Ominifood</div>
+              <div class="card__link">
+                <i class="fas fa-link"></i>
+              </div>
+              <div class="card__link">
+                <i class="fab fa-github"></i>
+              </div>
+            </div>
+            <div class="card__main">整合JQuery搭配animate.css製作網頁</div>
+          </div>
         </div>
-
+        <div class="col-1-of-3">
+          <div class="card">
+            <div class="card__img card__img--3"></div>
+            <div class="card__descript">
+              <div class="card__descript--title">Ominifood</div>
+              <div class="card__link">
+                <i class="fas fa-link"></i>
+              </div>
+              <div class="card__link">
+                <i class="fab fa-github"></i>
+              </div>
+            </div>
+            <div class="card__main">整合JQuery搭配animate.css製作網頁</div>
+          </div>
+        </div>
+        <div class="col-1-of-3">
+          <div class="card">
+            <div class="card__img card__img--2"></div>
+            <div class="card__descript">
+              <div class="card__descript--title">Ominifood</div>
+              <div class="card__link">
+                <i class="fas fa-link"></i>
+              </div>
+              <div class="card__link">
+                <i class="fab fa-github"></i>
+              </div>
+            </div>
+            <div class="card__main">整合JQuery搭配animate.css製作網頁</div>
+          </div>
+        </div>
+      </div>
+      <div class="row ">
+        <div class="col-1-of-3">
+          <div class="card">
+            <div class="card__img card__img--4"></div>
+            <div class="card__descript">
+              <div class="card__descript--title">Ominifood</div>
+              <div class="card__link">
+                <i class="fas fa-link"></i>
+              </div>
+              <div class="card__link">
+                <i class="fab fa-github"></i>
+              </div>
+            </div>
+            <div class="card__main">整合JQuery搭配animate.css製作網頁</div>
+          </div>
+        </div>
+        <div class="col-1-of-3">
+          <div class="card">
+            <div class="card__img card__img--5"></div>
+            <div class="card__descript">
+              <div class="card__descript--title">Ominifood</div>
+              <div class="card__link">
+                <i class="fas fa-link"></i>
+              </div>
+              <div class="card__link">
+                <i class="fab fa-github"></i>
+              </div>
+            </div>
+            <div class="card__main">整合JQuery搭配animate.css製作網頁</div>
+          </div>
+        </div>
+        <div class="col-1-of-3">
+          <div class="card">
+            <div class="card__img card__img--6"></div>
+            <div class="card__descript">
+              <div class="card__descript--title">Ominifood</div>
+              <div class="card__link">
+                <i class="fas fa-link"></i>
+              </div>
+              <div class="card__link">
+                <i class="fab fa-github"></i>
+              </div>
+            </div>
+            <div class="card__main">整合JQuery搭配animate.css製作網頁</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
